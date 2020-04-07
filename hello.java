d02c1b02a0d319098180c3f47c41fcc0198a845b
annaberry125 <62617891+annaberry125@users.noreply.github.com>
2020-04-07 11:30:41 -0400

    add an equation
    simple addition

--- hello.java
+++ hello.java
@@ -1,5 +1,19 @@
 public class hello{
 	
+	public void doSomethingCrazy() {
+	
+		int a = 5;
+		int b = 10;
+		a + a = b;
+	
+	
+	}
+	
+	
+	
+	
+	
+	
 	public static void main (String[] args){
 		System.out.println("Hello Git");
 	}
